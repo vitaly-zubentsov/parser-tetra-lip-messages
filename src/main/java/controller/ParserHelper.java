c74fac8840d2e30581df5b493f6fd14c56e12c6f
zubentsov.vv <fdsa@fasd>
2021-09-17 11:34:51 +0300

    Create test and methods for calculating lip altitude.

--- src/main/java/controller/ParserHelper.java
+++ src/main/java/controller/ParserHelper.java
@@ -8,7 +8,7 @@ public class ParserHelper {
 
     // To understand how calculate longitude from lipLongitude see clause 6.3.50, ETSI TS 100 392-18-1 V1.7.2 (2018-01)
     protected static String calculateLongitudeFromLipBits(String lipBinLongitude) {
-        double lipDecLongitude = Long.parseLong(lipBinLongitude.substring(1), 2);
+        int lipDecLongitude = Integer.parseInt(lipBinLongitude.substring(1), 2);
         double stepOfCalculating = 360.0 / 33554432;// 360 /(2 ^ 25);
         double longitude = lipDecLongitude * stepOfCalculating;
         BigDecimal roundLongitude = new BigDecimal(longitude).setScale(10, RoundingMode.HALF_UP);
@@ -22,18 +22,37 @@ public class ParserHelper {
 
     // To understand how calculate latitude from lipLatitude see clause 6.3.30, ETSI TS 100 392-18-1 V1.7.2 (2018-01)
     protected static String calculateLatitudeFromLipBits(String lipBinLatitude) {
-        double lipDecLatitude = Long.parseLong(lipBinLatitude.substring(1), 2);
+        int lipDecLatitude = Integer.parseInt(lipBinLatitude.substring(1), 2);
         double stepOfCalculating = 180.0 / 16777216; //180/(2^24)
         double latitude = lipDecLatitude * stepOfCalculating;
         BigDecimal roundLatitude = new BigDecimal(latitude).setScale(10, RoundingMode.HALF_UP);
         //define a positive or negative number
         if (lipBinLatitude.substring(0, 1).equalsIgnoreCase("0")) {
-            return roundLatitude.toString();
+            return lipBinLatitude;
         } else {
-            return "-" + roundLatitude;
+           return "-" + roundLatitude;
         }
     }
 
+    // To understand how calculate altitude from lipLatitude see clause 6.3.31, ETSI TS 100 392-18-1 V1.7.2 (2018-01)
+    protected static String calculateAltitudeFromLipBits(String lipBinAltitude) {
+        int lipDecAltitude = Integer.parseInt(lipBinAltitude.substring(1), 2);
+
+        //define altitude above  positive or negative number
+        if (lipBinAltitude.substring(0, 1).equalsIgnoreCase("1")) {
+            return "User define altitude";
+        } else if (lipDecAltitude == 0) {
+            return "Reserved";
+        } else if (lipDecAltitude < 1202) {
+            return Integer.toString(-201 + lipDecAltitude) + " m";
+        } else if (lipDecAltitude < 1927) {
+            return Integer.toString(1000 + (lipDecAltitude - 1201) * 2) + " m";
+        } else if (lipDecAltitude < 2047) {
+            return Integer.toString(2450 +(lipDecAltitude - 1926) * 75) + " m";
+        } else {
+            return "11525 m or more";
+        }
+    }
 
 
     protected static Integer convertBinToDec(String substring) {
--- src/test/java/controller/TestParserHelperMethods.java
+++ src/test/java/controller/TestParserHelperMethods.java
@@ -5,8 +5,7 @@ import org.junit.jupiter.api.Assertions;
 
 import java.math.BigDecimal;
 
-import static controller.ParserHelper.calculateLatitudeFromLipBits;
-import static controller.ParserHelper.calculateLongitudeFromLipBits;
+import static controller.ParserHelper.*;
 
 
 public class TestParserHelperMethods {
@@ -36,6 +35,27 @@ public class TestParserHelperMethods {
         Assertions.assertEquals("179.9999892712", calculateLongitudeFromLipBits(
                 "0111111111111111111111111"));
     }
+
+    @Test
+    public void testCalculateAltitudeFromLipBits() {
+
+        Assertions.assertEquals("User define altitude", calculateAltitudeFromLipBits("111111111111"));
+        Assertions.assertEquals("User define altitude", calculateAltitudeFromLipBits("100000000000"));
+
+        Assertions.assertEquals("Reserved", calculateAltitudeFromLipBits("000000000000"));
+
+        Assertions.assertEquals("-200 m", calculateAltitudeFromLipBits("000000000001"));
+        Assertions.assertEquals("1000 m", calculateAltitudeFromLipBits("010010110001"));
+
+        Assertions.assertEquals("1002 m", calculateAltitudeFromLipBits("010010110010"));
+        Assertions.assertEquals("2450 m", calculateAltitudeFromLipBits("011110000110"));
+
+        Assertions.assertEquals("2525 m", calculateAltitudeFromLipBits("011110000111"));
+        Assertions.assertEquals("11450 m", calculateAltitudeFromLipBits("011111111110"));
+
+        Assertions.assertEquals("11525 m or more", calculateAltitudeFromLipBits("011111111111"));
+    }
+
 
 
 }
